c1ccc6c094aa809450790eb6adaf07483688af5d
yoonwonsik160317 <wssesang@gmail.com>
2024-09-02 18:33:04 +0900

    0902commit

---
+++ js/about.d7899d6d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_project"]=self["webpackChunkvue_project"]||[]).push([[594],{603:function(u,e,n){n.r(e),n.d(e,{default:function(){return l}});var t=n(641);const r={class:"about"},a=(0,t.Lk)("h1",null,"This is an about page",-1),c=[a];function s(u,e){return(0,t.uX)(),(0,t.CE)("div",r,c)}var o=n(6262);const i={},f=(0,o.A)(i,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.d7899d6d.js.map
